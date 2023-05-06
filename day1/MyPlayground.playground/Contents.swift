16c2758d49b19f5619a8d981e6426e17a0ed6cb6
ptejas26 <ptejas.patelia@gmail.com>
2023-05-05 21:37:02 -0400

    Added First week changes

--- day1/MyPlayground.playground/Contents.swift
+++ day1/MyPlayground.playground/Contents.swift
@@ -1,3 +1,42 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//let test: Void? = nil
+//print(test)
+
+
+enum MyError: Error {
+    case myErrorCase
+}
+
+func doSomething() throws {
+    //return "Test"
+    //throw MyError.myErrorCase
+}
+print(try? doSomething())
+
+
+func returnsOptionalString() throws -> String? {
+    return nil
+}
+
+let string = try? returnsOptionalString()
+
+if string == nil {
+    // why is string nil? Is it due to an error? Or did the function execute successfully and we just got back nil without encountering any errors?
+    print("Inside")
+}
+
+
+
+let array: [Int] = [1, 2, 3, 4, 5]
+label: for item in array {
+    for itemNew in array {
+        for anotherNewItem in array {
+            if anotherNewItem == 4 {
+                break label
+            }
+        }
+    }
+}
